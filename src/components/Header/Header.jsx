985f781f40728e4ce44422587f9724c1f83fc8c3
Cristian Albertino de Lira Filho <cristianfilho2008@outlook.com>
2026-01-29 23:53:00 -0300

    feat(i18n): adicionar botão de mudar de idioma portugues, ingles e espanhol utilizando a biblioteca i18n

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,7 +1,99 @@
 import styles from './Header.module.css';
+import imgEN from "../../assets/images/eua.png";
+import imgPT from "../../assets/images/brasil.png";
+import imgES from "../../assets/images/espanha.png";
+import { useTranslation } from 'react-i18next';
+import i18n from '../../i18n';
+import { useState } from 'react';
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { fas } from "@fortawesome/free-solid-svg-icons";
+
+
 function Header() {
+    const { t, i18n } = useTranslation();
+    const [isOpen, setIsOpen] = useState(false);
+
+    const abrirModal = () => {
+        // Lógica para abrir o modal de seleção de idioma
+        setLanguage(prev => (prev === 'brasil' ? 'eua' : 'brasil'));
+    }
+
+const languages = [
+  { code: "pt", name: "Português", flag: imgPT },
+  { code: "en", name: "English", flag: imgEN },
+  { code: "es", name: "Español", flag: imgES },
+];
+
+    const currentLanguage =
+        languages.find((l) => l.code === i18n.language) || languages[1];
+    
+    library.add(fas);
+
     return (
-        <header className={styles.Header}>This is the Header</header>
+      <header className={styles.SectionHeader}>
+        <div className={styles.Logo}>
+          <h5>Cristian Lira</h5>
+        </div>
+        <nav className={styles.Nav}>
+          <ul>
+            <li>
+              <a href="#home">{t("header.home")}</a>
+            </li>
+            <li>
+              <a href="#about">{t("header.about")}</a>
+            </li>
+            <li>
+              <a href="#skills">{t("header.skills")}</a>
+            </li>
+            <li>
+              <a href="#projects">{t("header.projects")}</a>
+            </li>
+            <li>
+              <a href="#contact">{t("header.contact")}</a>
+            </li>
+          </ul>
+        </nav>
+        <div className={styles.LanguageContainer}>
+          {/* Botão Principal do Select */}
+          <button
+            className={styles.SelectBtn}
+            onClick={() => setIsOpen(!isOpen)}
+          >
+            <img src={currentLanguage.flag} alt="" width="20" />
+
+            <span>{currentLanguage.name}</span>
+
+            <i className={`${styles.Arrow} ${isOpen ? styles.Open : ""}`}>
+              <FontAwesomeIcon icon={["fas", "angle-down"]} />
+            </i>
+          </button>
+
+          {/* Pop-up (Menu Dropdown) */}
+          {isOpen && (
+            <ul className={styles.Dropdown}>
+              {languages.map((lang) => (
+                <li key={lang.code}>
+                  <button
+                    className={
+                      lang.code === i18n.language
+                        ? styles.Selected
+                        : styles.NotSelected
+                    }
+                    onClick={() => {
+                      i18n.changeLanguage(lang.code);
+                      setIsOpen(false);
+                    }}
+                  >
+                    <img src={lang.flag} alt="" width="20" />
+                    {lang.name}
+                  </button>
+                </li>
+              ))}
+            </ul>
+          )}
+        </div>
+      </header>
     );
 }
 
---
+++ src/i18n/index.js
@@ -0,0 +1,24 @@
+import i18n from "i18next";
+import { initReactI18next } from "react-i18next";
+import LanguageDetector from "i18next-browser-languagedetector";
+
+import ptTranslation from "./locales/pt/translation.json";
+import enTranslation from "./locales/en/translation.json";
+import esTranslation from "./locales/es/translation.json";
+
+i18n
+  .use(LanguageDetector) // Detecta o idioma do navegador
+  .use(initReactI18next) // Passa o i18n para o react-i18next
+  .init({
+    resources: {
+      pt: { translation: ptTranslation },
+      en: { translation: enTranslation },
+      es: { translation: esTranslation },
+    },
+    fallbackLng: "en", // Se não detectar nada, usa PT
+    interpolation: {
+      escapeValue: false,
+    },
+  });
+
+export default i18n;
