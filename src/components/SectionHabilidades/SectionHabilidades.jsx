1e6711f87ecefe67c440d5c61f52b9072a4b772c
Cristian Albertino de Lira Filho <cristianfilho2008@outlook.com>
2026-02-07 23:10:43 -0300

    style(Habilidades): ajustar nomes e adicionar divisor waves

--- src/components/SectionHabilidades/SectionHabilidades.jsx
+++ src/components/SectionHabilidades/SectionHabilidades.jsx
@@ -66,8 +66,8 @@ function SectionHabilidades() {
     MySQL: { icon: <SiMysql />, class: "mysql" },
     "API REST": { icon: <TbApi />, class: "api" },
     "Axios": { icon: <TbApi />, class: "axios" },
-    "Git (Versionamento)": { icon: <FaGitAlt />, class: "git" },
-    "Vite (Build Tool)": { icon: <img src={vite} alt="Vite" />, class: "vite" },
+    "Git": { icon: <FaGitAlt />, class: "git" },
+    "Vite": { icon: <img src={vite} alt="Vite" />, class: "vite" },
     "Vercel (Deploy)": { icon: <img src={vercel} alt="Vercel" />, class: "vercel" },
   };
 
@@ -185,7 +185,12 @@ function SectionHabilidades() {
         </div>
       )}
 
-      <div className={styles.divisor}></div>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+        <path
+          fill="#1c1f251"
+          d="M0,128L34.3,133.3C68.6,139,137,149,206,133.3C274.3,117,343,75,411,69.3C480,64,549,96,617,101.3C685.7,107,754,85,823,101.3C891.4,117,960,171,1029,165.3C1097.1,160,1166,96,1234,80C1302.9,64,1371,96,1406,112L1440,128L1440,320L1405.7,320C1371.4,320,1303,320,1234,320C1165.7,320,1097,320,1029,320C960,320,891,320,823,320C754.3,320,686,320,617,320C548.6,320,480,320,411,320C342.9,320,274,320,206,320C137.1,320,69,320,34,320L0,320Z"
+        ></path>
+      </svg>
     </section>
   );
 }
