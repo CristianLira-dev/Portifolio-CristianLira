89f761c56f17e5efa3d6a295396a9ee90c40bcca
Cristian Albertino de Lira Filho <cristianfilho2008@outlook.com>
2026-02-08 00:19:27 -0300

    feat(projetos): adicionar Section dos projetos

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import Divisor from './components/Divisor/Divisor.jsx';
 import Header from './components/Header/Header.jsx';
 import SectionHabilidades from './components/SectionHabilidades/SectionHabilidades.jsx';
 import SectionHero from './components/SectionHero/SectionHero.jsx';
+import SectionProjetos from './components/SectionProjetos/SectionProjetos.jsx';
 import SectionSobre from './components/SectionSobre/SectionSobre.jsx';
 
 function App() {
@@ -13,6 +14,7 @@ function App() {
       < Divisor />
       <SectionSobre />
       <SectionHabilidades />
+      <SectionProjetos />
     </main>
   )
 }
---
+++ src/components/SectionProjetos/SectionProjetos.jsx
@@ -0,0 +1,11 @@
+import styles from './SectionProjetos.module.css';
+
+function SectionProjetos() {
+    return (  
+        <section className={styles.projetos} id="projects">
+            <h2>Meus Projetos</h2>
+            </section>
+    );
+}
+
+export default SectionProjetos;
